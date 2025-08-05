4c121230831dd21be65b276166a94a18bd21503e
Pratham90123 <achauhanprathamz84@gmail.com>
2025-08-05 15:51:20 +0530

    created a constroctor

--- student.java
+++ student.java
@@ -1,3 +1,19 @@
 public class student {
     
+    int rollno;
+    String name;
+    int age;
+    String course;
+
+
+    public student(int rollno,String name,int age,String course)
+    {
+        this.rollno = rollno;
+        this.name=name;
+        this.age=age;
+        this.course=course;
+    }
+    public void display(){
+        System.out.println("rollno"rollno,"Name",name,"Age",age,"course",course);
+    }
 }
